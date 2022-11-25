e64098945655be9c6c04fa40dc37d6a41e7f446b
Andrew Kopti <andrewkopti@gmail.com>
2022-11-25 16:39:55 -0500

    added config and text highlighting

--- app.py
+++ app.py
@@ -3,11 +3,31 @@ import pandas as pd
 import altair as alt
 import plotly.express as px
 
-# header
-st.header('Non-profit Organizations in the United States')
+# css used for highlighting text
+highlight_css = """
+    <style>
+        .highlight {
+            background-color: #dbfaf7;
+            border-radius: 7px;
+            padding: 4px;
+            font-weight: 550;
+        }
+        .title {
+            background-color: #1b3652;
+            border-radius: 10px;
+            padding: 5px;
+            color: #ffffff;
+            font-weight: 550;
+        }
+    </style>
+"""
+st.markdown(highlight_css, unsafe_allow_html=True)
+
+# title
+st.write('# <span class=title>Non-profit</span> Organizations in the United States', unsafe_allow_html=True)
 
 # sidebar
-st.sidebar.header('Size Metric')
+st.sidebar.header('Size')
 st.sidebar.write('How do you define an organization\'s size? Select the metric below that\'s most important to you.')
 metrics = {
     'Employees': 'TOTEMPLOYEE', 
@@ -19,7 +39,17 @@ n_organizations = st.sidebar.slider('Number of organizations to view:', 3, 25, 1
 # data
 df = pd.read_csv('data.csv')
 total_organizations = df.shape[0]
-total_revenue = int(df['TOTALREVCURRENT'].sum())
+
+# function sourced from https://stackoverflow.com/questions/579310/formatting-long-numbers-as-strings/45846841#45846841
+def human_format(num):
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', ' Thousand', ' Million', ' Billion'][magnitude])  
+
+total_revenue = human_format(df['TOTALREVCURRENT'].sum())
 
 # organization count dataframe
 count_df = df.groupby(['STATE'])['STATE'].count()
@@ -42,10 +72,10 @@ tab1, tab2, tab3 = st.tabs(['Exploring the U.S.', 'Largest Non-profits', 'Non-pr
 
 with tab1:
     # text
-    st.write('### How big is the non-profit industry in the United States?')
-    st.write(f'In 2017, **{total_organizations:,}** non-profit orgnanizations \
-        were registered in the United States.')
-    st.write(f'They reported **${total_revenue:,}** in total revenue.')
+    st.header('How big is the non-profit industry in the United States?')
+    st.write(f'In 2017, <span class = highlight>{total_organizations:,}</span> non-profit orgnanizations \
+        were registered in the United States.', unsafe_allow_html=True)
+    st.write(f'They reported <span class = highlight>${total_revenue}</span> in total revenue.', unsafe_allow_html=True)
 
     # choropleth using plotly (streamlit does not render these from altair)
     count_map = px.choropleth(
@@ -69,13 +99,14 @@ with tab1:
 
 with tab2:
     # text
-    st.write('### Who are the largest non-profits in the United States?')
-    st.write('By revenue, the top 10 non-profit organizations are part of \
-        the healthcare industry. Four of the top 10 are located in California.')
-    st.write('By employee count, eight of the top 10 are also healthcare \
-        organizations. The two exceptions are the YMCA, which provides community \
-        services aimed at youths, and the Good Samaritan Society, which provides \
-        senior housing services.')
+    st.header('Who are the largest non-profits in the United States?')
+    st.write('By revenue, <span class = highlight>the top 10 non-profit \
+        organizations are part of the healthcare industry.</span> \
+        Four of the top 10 are located in California.', unsafe_allow_html=True)
+    st.write('By employee count, <span class = highlight>eight of the top 10 \
+        are also healthcare organizations</span>. The two exceptions are the \
+        YMCA, which provides community services aimed at youths, and the Good \
+        Samaritan Society, which provides senior housing services.', unsafe_allow_html=True)
     st.write('By number of volunteers, the organizations in the top 10 have a much \
         more diverse mix of missions.')
 
@@ -116,7 +147,7 @@ with tab2:
 
 with tab3:
     # text
-    st.write('### How do the largest non-profits spend their money?')
+    st.header('How do the largest non-profits spend their money?')
     st.write('Non-profits with different missions can be expected to spend different \
         proportions of their revenue on salaries versus other expenses.')
     st.write('For example, a hospital system needs to employ a large number \
@@ -131,7 +162,7 @@ with tab3:
         x=alt.X('salary_proportion:Q', axis=alt.Axis(format='%', title='Revenue Used for Salaries')),
         y=alt.Y(f'NAME:O', sort='-x', axis=alt.Axis(title=None)),
         color=alt.Color('salary_proportion', scale=alt.Scale(scheme='browns'), legend=None),
-        tooltip=alt.Tooltip('salary_proportion:Q', format='.1%')
+        tooltip=alt.Tooltip('salary_proportion:Q', format='.0%')
     ).properties(
         width='container',
         height=350
